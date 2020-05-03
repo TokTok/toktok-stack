1247b25df955c3e127008750ebaa17815cd99c7a
Restyled.io <commits@restyled.io>
2020-05-03 10:43:04 +0000

    Restyled by autopep8

--- tools/project/cabal_test.py
+++ tools/project/cabal_test.py
@@ -5,8 +5,10 @@ import json
 import re
 import sys
 
+
 def parse_bazel(build):
     data = {}
+
     def store(rule):
         def f(name, **kwargs):
             data[name] = kwargs
@@ -26,8 +28,9 @@ def parse_bazel(build):
     })
     return json.loads(json.dumps(data))
 
+
 def parse_cabal(cabal):
-    rec_dd = lambda: collections.defaultdict(rec_dd)
+    def rec_dd(): return collections.defaultdict(rec_dd)
     data = rec_dd()
     section = data['']['']
     key = None
@@ -60,13 +63,14 @@ def parse_cabal(cabal):
             key = None
     return json.loads(json.dumps(data))
 
+
 def resolve_bazel_deps(bazel, data, name):
     def go(name):
         deps = []
         rule = data.get(name, None)
         if not rule:
             print("missing rule for '{name}' in {bazel}".format(
-                name = name, bazel = bazel))
+                name=name, bazel=bazel))
             sys.exit(1)
         for dep in rule["deps"]:
             if dep.startswith(":hs-"):
@@ -74,7 +78,7 @@ def resolve_bazel_deps(bazel, data, name):
             elif dep.startswith("//hs-"):
                 deps.append(dep[5:])
             elif dep.startswith("//c-"):
-                pass # ignore native deps for now
+                pass  # ignore native deps for now
             elif dep.startswith(":"):
                 if name != "testsuite":
                     deps.extend(go(dep[1:]))
@@ -83,54 +87,58 @@ def resolve_bazel_deps(bazel, data, name):
         return deps
     return set(go(name))
 
+
 def must_list(maybe_list):
     if isinstance(maybe_list, list):
         return maybe_list
     else:
         return [maybe_list]
 
+
 def check_deps(bazel, cabal, bazel_data, bazel_name, cabal_section):
     bazel_deps = resolve_bazel_deps(bazel, bazel_data, bazel_name)
     cabal_deps = {dep.split(" ")[0]
-            for dep in must_list(cabal_section["build-depends"])}
+                  for dep in must_list(cabal_section["build-depends"])}
 
     cabal_missing = sorted(bazel_deps - cabal_deps)
     section = cabal_section['__name__']
     if cabal_missing:
         print("{cabal} is missing deps for section "
-                "'{section}' from {bazel} '{bazel_name}': "
-                "{cabal_missing}".format(
-                    cabal = cabal,
-                    section = section,
-                    bazel = bazel,
-                    bazel_name = bazel_name,
-                    cabal_missing = cabal_missing,
-                ))
+              "'{section}' from {bazel} '{bazel_name}': "
+              "{cabal_missing}".format(
+                  cabal=cabal,
+                  section=section,
+                  bazel=bazel,
+                  bazel_name=bazel_name,
+                  cabal_missing=cabal_missing,
+              ))
         sys.exit(1)
 
     bazel_missing = sorted(cabal_deps - bazel_deps)
     if bazel_missing:
         print("{bazel} is missing deps for '{bazel_name}' from "
-                "{cabal} section '{section}: "
-                "{bazel_missing}".format(
-                    bazel = bazel,
-                    bazel_name = bazel_name,
-                    cabal = cabal,
-                    section = section,
-                    bazel_missing = bazel_missing,
-                ))
+              "{cabal} section '{section}: "
+              "{bazel_missing}".format(
+                  bazel=bazel,
+                  bazel_name=bazel_name,
+                  cabal=cabal,
+                  section=section,
+                  bazel_missing=bazel_missing,
+              ))
         sys.exit(1)
+
 
 def check_version(root, bazel_data, package_name):
     cabal_version = root["version"]
     bazel_version = bazel_data["hs-" + package_name]["version"]
     if cabal_version != bazel_version:
         print("bazel version ({bazel_version}) and cabal version "
-                "({cabal_version}) disagree".format(
-                    bazel_version = bazel_version,
-                    cabal_version = cabal_version,
-                ))
+              "({cabal_version}) disagree".format(
+                  bazel_version=bazel_version,
+                  cabal_version=cabal_version,
+              ))
         sys.exit(1)
+
 
 def main(args):
     bazel, cabal = args
@@ -142,10 +150,11 @@ def main(args):
     package_name = root["name"]
 
     check_deps(bazel, cabal, bazel_data,
-            "hs-" + package_name, cabal_data["library"][""])
+               "hs-" + package_name, cabal_data["library"][""])
     check_deps(bazel, cabal, bazel_data,
-            "testsuite", cabal_data["test-suite"]["testsuite"])
+               "testsuite", cabal_data["test-suite"]["testsuite"])
 
     check_version(root, bazel_data, package_name)
+
 
 main(sys.argv[1:])
