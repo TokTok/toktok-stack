e219f71f1b717952b57306442aa8ef08cfb265ae
Restyled.io <commits@restyled.io>
2020-05-03 11:19:28 +0000

    Restyled by astyle

--- tools/debug/mallocfail.c
+++ tools/debug/mallocfail.c
@@ -5,22 +5,22 @@
 // This "malloc" library is empty apart from this initialiser, just to have a
 // timeout for tests, because mallocfail doesn't terminate otherwise.
 static __attribute__((__constructor__)) void init(void) {
-  long timeout = 0;
+    long timeout = 0;
 
-  const char *env_timeout = getenv("MALLOCFAIL_TIMEOUT");
-  if (env_timeout != NULL) {
-    char *end = NULL;
-    timeout = strtol(env_timeout, &end, 10);
-    if (end == NULL || *end != '\0') {
-      fprintf(stderr, "mallocfail: invalid value for MALLOCFAIL_TIMEOUT: %s\n",
-              env_timeout);
-      abort();
+    const char *env_timeout = getenv("MALLOCFAIL_TIMEOUT");
+    if (env_timeout != NULL) {
+        char *end = NULL;
+        timeout = strtol(env_timeout, &end, 10);
+        if (end == NULL || *end != '\0') {
+            fprintf(stderr, "mallocfail: invalid value for MALLOCFAIL_TIMEOUT: %s\n",
+                    env_timeout);
+            abort();
+        }
     }
-  }
 
-  // If a timeout was not set, we may be running in a debugger, and don't want
-  // a timeout at all.
-  if (timeout != 0) {
-    alarm(timeout);
-  }
+    // If a timeout was not set, we may be running in a debugger, and don't want
+    // a timeout at all.
+    if (timeout != 0) {
+        alarm(timeout);
+    }
 }
