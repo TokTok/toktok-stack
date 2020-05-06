e0af86811e6810b6851cb38302b5944e03acf2f7
Restyled.io <commits@restyled.io>
2020-05-06 20:56:42 +0000

    Restyled by astyle

--- tools/debug/flakynet.c
+++ tools/debug/flakynet.c
@@ -38,48 +38,48 @@ static double flakiness = 0.2;
 static bool verbose = false;
 
 static __attribute__((__constructor__)) void init(void) {
-  const char *env_seed = getenv("FLAKYNET_SEED");
-  if (env_seed != NULL) {
-    char *end = NULL;
-    seed = strtol(env_seed, &end, 10);
-    if (end == NULL || *end != '\0') {
-      fprintf(stderr, "flakynet: invalid value for FLAKYNET_SEED: %s\n",
-              env_seed);
-      abort();
+    const char *env_seed = getenv("FLAKYNET_SEED");
+    if (env_seed != NULL) {
+        char *end = NULL;
+        seed = strtol(env_seed, &end, 10);
+        if (end == NULL || *end != '\0') {
+            fprintf(stderr, "flakynet: invalid value for FLAKYNET_SEED: %s\n",
+                    env_seed);
+            abort();
+        }
     }
-  }
 
-  const char *env_flakiness = getenv("FLAKYNET_FLAKINESS");
-  if (env_flakiness != NULL) {
-    char *end = NULL;
-    flakiness = strtod(env_flakiness, &end);
-    if (end == NULL || *end != '\0') {
-      fprintf(stderr, "flakynet: invalid value for FLAKYNET_FLAKINESS: %s\n",
-              env_flakiness);
-      abort();
+    const char *env_flakiness = getenv("FLAKYNET_FLAKINESS");
+    if (env_flakiness != NULL) {
+        char *end = NULL;
+        flakiness = strtod(env_flakiness, &end);
+        if (end == NULL || *end != '\0') {
+            fprintf(stderr, "flakynet: invalid value for FLAKYNET_FLAKINESS: %s\n",
+                    env_flakiness);
+            abort();
+        }
     }
-  }
 
-  const char *env_verbose = getenv("FLAKYNET_VERBOSE");
-  verbose = env_verbose != NULL && *env_verbose == '1';
+    const char *env_verbose = getenv("FLAKYNET_VERBOSE");
+    verbose = env_verbose != NULL && *env_verbose == '1';
 }
 
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
                const struct sockaddr *dest_addr, socklen_t addrlen) {
-  typedef __typeof__(sendto) sendto_type;
-  static sendto_type *libc_sendto;
-  if (libc_sendto == NULL) {
-    libc_sendto = (sendto_type *)dlsym(RTLD_NEXT, "sendto");
-  }
-
-  if ((double)rand_r(&seed) / (double)RAND_MAX < flakiness) {
-    // Success, but we don't actually send.
-    if (len > 0 && verbose) {
-      fprintf(stderr,
-              "flakynet: dropping packet of length %zu, packet[0] = 0x%02x\n",
-              len, ((const char *)buf)[0]);
+    typedef __typeof__(sendto) sendto_type;
+    static sendto_type *libc_sendto;
+    if (libc_sendto == NULL) {
+        libc_sendto = (sendto_type *)dlsym(RTLD_NEXT, "sendto");
     }
-    return len;
-  }
-  return libc_sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+
+    if ((double)rand_r(&seed) / (double)RAND_MAX < flakiness) {
+        // Success, but we don't actually send.
+        if (len > 0 && verbose) {
+            fprintf(stderr,
+                    "flakynet: dropping packet of length %zu, packet[0] = 0x%02x\n",
+                    len, ((const char *)buf)[0]);
+        }
+        return len;
+    }
+    return libc_sendto(sockfd, buf, len, flags, dest_addr, addrlen);
 }
