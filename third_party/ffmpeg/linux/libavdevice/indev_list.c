e187e7161c6f1debe5f47866b0924a2a9bbd2ba4
Restyled.io <commits@restyled.io>
2021-12-03 14:55:55 +0000

    Restyled by clang-format

--- third_party/ffmpeg/linux/libavdevice/indev_list.c
+++ third_party/ffmpeg/linux/libavdevice/indev_list.c
@@ -1,7 +1,2 @@
-static const AVInputFormat * const indev_list[] = {
-    &ff_alsa_demuxer,
-    &ff_lavfi_demuxer,
-    &ff_oss_demuxer,
-    &ff_xcbgrab_demuxer,
-    NULL
-};
+static const AVInputFormat* const indev_list[] = {&ff_alsa_demuxer, &ff_lavfi_demuxer,
+                                                  &ff_oss_demuxer, &ff_xcbgrab_demuxer, NULL};
--- third_party/ffmpeg/linux/libavdevice/outdev_list.c
+++ third_party/ffmpeg/linux/libavdevice/outdev_list.c
@@ -1,5 +1,1 @@
-static const AVOutputFormat * const outdev_list[] = {
-    &ff_alsa_muxer,
-    &ff_oss_muxer,
-    NULL
-};
+static const AVOutputFormat* const outdev_list[] = {&ff_alsa_muxer, &ff_oss_muxer, NULL};
