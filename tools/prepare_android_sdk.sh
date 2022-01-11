11906f30bab8928ecaf6535aa677d44f13a51ba8
iphydf <iphydf@users.noreply.github.com>
2022-01-11 22:24:46 +0000

    chore: Enable layering check on third party build files.

--- tools/prepare_android_sdk.sh
+++ tools/prepare_android_sdk.sh
@@ -7,7 +7,7 @@ if [ ! -d third_party/android/sdk/cmdline-tools ]; then
   mkdir -p third_party/android/sdk/
   unzip -q -d third_party/android/sdk/ commandlinetools-linux-7583922_latest.zip
   rm commandlinetools-linux-7583922_latest.zip
-  yes | third_party/android/sdk/cmdline-tools/bin/sdkmanager --sdk_root=third_party/android/sdk --licenses
+  yes | third_party/android/sdk/cmdline-tools/bin/sdkmanager --sdk_root=third_party/android/sdk --licenses >/dev/null
   touch "$HOME/.android/repositories.cfg"
   third_party/android/sdk/cmdline-tools/bin/sdkmanager --sdk_root=third_party/android/sdk "tools"
   third_party/android/sdk/cmdline-tools/bin/sdkmanager --sdk_root=third_party/android/sdk "build-tools;30.0.0"
