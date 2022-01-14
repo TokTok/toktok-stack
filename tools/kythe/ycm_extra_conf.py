20348574ca953f1c7da3cc36ed3c3c9c72e8ec16
Restyled.io <commits@restyled.io>
2022-01-14 19:27:39 +0000

    Restyled by yapf

--- tools/kythe/ycm_extra_conf.py
+++ tools/kythe/ycm_extra_conf.py
@@ -13,7 +13,9 @@ logging.basicConfig(level=logging.DEBUG)
 
 # pylint: disable=invalid-name
 def Settings(
-    language: str, filename: Optional[str] = None, client_data: Optional[str] = None,
+        language: str,
+        filename: Optional[str] = None,
+        client_data: Optional[str] = None,
 ) -> Dict[str, List[str]]:
     """Return flags for the file loaded into vim."""
     del client_data
@@ -27,9 +29,8 @@ def Settings(
     db = bazel.generate_compilation_database([filename])
     if not db:
         raise Exception(f"unable to build compilation database for {filename}")
-    flags = builder.flags_for_clang(
-        bazel.execution_root(), shlex.split(db[0]["command"])[1:]
-    )
+    flags = builder.flags_for_clang(bazel.execution_root(),
+                                    shlex.split(db[0]["command"])[1:])
     return {"flags": flags}
 
 
