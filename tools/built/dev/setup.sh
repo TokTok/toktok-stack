21fed867fcde6846e7672ece0844d51e202e303d
Restyled.io <commits@restyled.io>
2022-01-30 02:41:14 +0000

    Restyled by shfmt

--- tools/built/dev/setup.sh
+++ tools/built/dev/setup.sh
@@ -4,7 +4,7 @@ set -eux
 
 git clone --depth=1 https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 vi --not-a-term +PluginInstall +qall
-cat ~/.vim/settings.vim >> ~/.vimrc
+cat ~/.vim/settings.vim >>~/.vimrc
 
 bazel-compdb
 bazel build --show_timestamps //...
