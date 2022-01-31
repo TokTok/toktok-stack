633a71ab461af2553f8452a6aff5bc0d639d2bc2
iphydf <iphydf@users.noreply.github.com>
2022-01-31 00:31:49 +0000

    fix: Install a default .bazelrc.local on init.
    When the user doesn't have one, builds will fail because the config
    doesn't know which compiler is used.

--- tools/built/src/init.sh
+++ tools/built/src/init.sh
@@ -14,6 +14,9 @@ if [ ! -d third_party/javacpp/ffmpeg/jar ]; then
   tools/prepare_third_party.sh
 fi
 
+# Install a default .bazelrc.local that works with the dev container.
+if [ ! -f .bazelrc.local ]; then ln -s .bazelrc.local.example .bazelrc.local; fi
+
 if [ -d ~/.vscode-server ]; then sudo chown -R builder:users ~/.vscode-server; fi
 if [ -d /src/workspace/.vscode ]; then sudo chown -R builder:users /src/workspace/.vscode; fi
 
