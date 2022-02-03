ca32d6773e083494f88248fc8b8fca519d684773
iphydf <iphydf@users.noreply.github.com>
2022-02-03 10:48:54 +0000

    feat: Install manpages in dev container.

--- tools/built/dev/deploy_dev_container.sh
+++ tools/built/dev/deploy_dev_container.sh
@@ -3,8 +3,11 @@
 set -eux
 
 IMAGE="$1"
+shift
 
-docker pull toxchat/toktok-stack:latest-dev
+if [ "$@" = "-u" ]; then
+  docker pull toxchat/toktok-stack:latest-dev
+fi
 docker build -t "$IMAGE" -f workspace/tools/built/dev/Dockerfile home
 sudo systemctl restart docker-toktok
 sudo journalctl -f -u docker-toktok
