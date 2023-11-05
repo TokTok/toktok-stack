693e3949d4a4c7e71c979d4dd6eab823ded16edd
Restyled.io <commits@restyled.io>
2023-11-05 23:14:54 +0000

    Restyled by shfmt

--- tools/built/src/setup-local.sh
+++ tools/built/src/setup-local.sh
@@ -3,7 +3,7 @@
 set -eux
 
 sudo nix-daemon --daemon &
-sleep 1  # wait for daemon to arrive (race, but *shrug*)
+sleep 1 # wait for daemon to arrive (race, but *shrug*)
 
 if grep "BEGIN" ~/key.pem; then
   nix-shell -p gnupg --run "gpg --import ~/key.pem"
