578a0dde0f943e487a7f09d9961619030d9f4a53
Restyled.io <commits@restyled.io>
2023-12-29 20:44:10 +0000

    Restyled by clang-format

--- third_party/zig/csource.c
+++ third_party/zig/csource.c
@@ -6,10 +6,10 @@
 static char answer_data[sizeof(int)];
 
 int get_answer(int question) {
-    int previous_answer;
-    memcpy(&previous_answer, answer_data, sizeof(int));
-    explicit_bzero(answer_data, sizeof(int));
-    memcpy(answer_data, &previous_answer, sizeof(int));
+  int previous_answer;
+  memcpy(&previous_answer, answer_data, sizeof(int));
+  explicit_bzero(answer_data, sizeof(int));
+  memcpy(answer_data, &previous_answer, sizeof(int));
 
-    return previous_answer;
+  return previous_answer;
 }
