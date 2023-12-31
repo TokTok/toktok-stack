2b463d016c124bcb4a53f2598bad3d9a85191126
Restyled.io <commits@restyled.io>
2023-12-31 17:07:13 +0000

    Restyled by clang-format

--- third_party/zig/csource.c
+++ third_party/zig/csource.c
@@ -7,17 +7,17 @@
 static char answer_data[sizeof(int)];
 
 int get_answer(int question) {
-    int previous_answer;
-    memcpy(&previous_answer, answer_data, sizeof(int));
-    // Not available in the default zig toolchain, so tests whether we have the
-    // correct glibc.
-    explicit_bzero(answer_data, sizeof(int));
-    memcpy(answer_data, &previous_answer, sizeof(int));
+  int previous_answer;
+  memcpy(&previous_answer, answer_data, sizeof(int));
+  // Not available in the default zig toolchain, so tests whether we have the
+  // correct glibc.
+  explicit_bzero(answer_data, sizeof(int));
+  memcpy(answer_data, &previous_answer, sizeof(int));
 
-    // Check that we can link against libsodium.
-    if (question >= randombytes_uniform(42)) {
-        return 42;
-    }
+  // Check that we can link against libsodium.
+  if (question >= randombytes_uniform(42)) {
+    return 42;
+  }
 
-    return previous_answer + 23;
+  return previous_answer + 23;
 }
